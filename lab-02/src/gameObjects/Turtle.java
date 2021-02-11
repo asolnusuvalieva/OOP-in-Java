1d2029b79bd541a0a894d9b0d23f920387e0117f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-11 23:18:54 +0600

    Do Problem01 in lab-02 (Turtle Implementation)

---
+++ lab-02/src/gameObjects/Turtle.java
@@ -0,0 +1,76 @@
+package gameObjects;
+
+public class Turtle {
+    //Properties
+    private int x = 0, y = 0;
+    private int dx = 1, dy = 0;
+    private boolean isPenDown = false;
+    private Field field;
+
+    //Getter and Setter methods
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+
+    //YAGNI
+    public void setX(int newX){
+        if (Field.areCoordsInside(newX, y)){
+            x = newX;
+        }
+    }
+    //YAGNI
+    public void setY(int newY){
+        if (Field.areCoordsInside(x, newY)){
+            y = newY;
+        }
+    }
+
+    //Methods
+    public Turtle(Field newField){
+        field = newField;
+    }
+    public void putPenUp(){
+        isPenDown = false;
+    }
+
+    public void putPenDown(){
+        isPenDown = true;
+    }
+
+    public void turnRight(){
+        int temp = dx;
+        dx = dy;
+        dy = temp;
+
+        dx = -dx;
+    }
+
+    public void turnLeft(){
+        int temp = dx;
+        dx = dy;
+        dy = temp;
+
+        dy = -dy;
+    }
+
+    public void move(int steps){
+        for(int i = 0; i < steps; i++){
+            int nextX = x + dx;
+            int nextY = y + dy;
+
+            if(!Field.areCoordsInside(nextX, nextY)){
+                break;
+            }
+
+            if(isPenDown){
+                field.putMark(x, y);
+            }
+            x = nextX;
+            y = nextY;
+        }
+    }
+
+}
