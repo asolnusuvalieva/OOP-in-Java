c184cf0c91064e15eee4837bd98c8d68a812a94a
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-11 23:19:15 +0600

    Do Problem01 in lab-02 (Field Implementation)

---
+++ lab-02/src/gameObjects/Field.java
@@ -0,0 +1,47 @@
+package gameObjects;
+
+public class Field {
+    //Properties
+    private static final char TURTLE_CELL = 'T';
+    private static final char EMPTY_CELL = '.';
+    private static final char MARKED_CELL = '*';
+
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 20;
+
+    private char[][] content;
+
+    //Methods
+    public Field(){
+        content = new char[HEIGHT][WIDTH];
+        for(int i = 0; i < HEIGHT; i++){
+            for (int j = 0; j < WIDTH; j++){
+                content[i][j] = EMPTY_CELL;
+            }
+        }
+    }
+
+    public static boolean areCoordsInside(int x, int y){
+        return x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT;
+    }
+
+    public void putMark(int x, int y){
+        if (areCoordsInside(x, y)){
+            content[y][x] = MARKED_CELL;
+        }
+    }
+
+    public void display(Turtle turtle){
+        for(int i = 0; i < HEIGHT; i++){
+            for (int j = 0; j < WIDTH; j++){
+                if(turtle.getY() == i && turtle.getX() == j){
+                    System.out.print(TURTLE_CELL);
+                }else{
+                    System.out.print(content[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
