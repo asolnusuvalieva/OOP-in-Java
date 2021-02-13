b9d10c67d4312e7082ced3c526ef5771c52dc74e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-13 22:14:31 +0600

    Do URI1257

---
+++ uri/src/URI1257.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class URI1257 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int testCases = scanner.nextInt();
+        scanner.nextLine(); //TODO: To avoid misreading
+
+        for(int i = 0; i < testCases; i++){
+            int numberOfLines = scanner.nextInt();
+            scanner.nextLine(); //TODO: To avoid misreading
+            int hash = 0;
+
+            for(int j = 0; j < numberOfLines; j++){
+                String line = scanner.nextLine();
+                int elementOfInput = j;
+
+                //Working with each character of every line
+                for(int c = 0; c < line.length(); c++){
+                    int positionInElement = c;
+                    int alphabetPosition = alphabet.indexOf(line.charAt(c));
+                    hash += alphabetPosition + elementOfInput + positionInElement;
+                }
+            }
+
+            System.out.println(hash);
+        }
+    }
+}
+//Accepted and pushed
