c250a2815c0d6a1518df00127730214ad22aec8d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-18 23:09:16 +0600

    Do URI1234

---
+++ uri/src/URI1234.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/*
+A sentence is called dancing if its first letter is uppercase
+and the case of each subsequent letter is the opposite of the previous letter.
+
+For example, AsOl or HeLlO
+
+*Spaces should be ignored
+ */
+public class URI1234 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+            String input = scanner.nextLine();
+            int count = 0;
+            String output = "";
+            for(int i = 0; i < input.length(); i++){
+                char currentChar = input.charAt(i);
+
+                if(count % 2 == 0){
+                    output += Character.toUpperCase(currentChar);
+                }else{
+                    output += Character.toLowerCase(currentChar);
+                }
+
+                if(currentChar != ' '){
+                    count++;
+                }
+            }
+            System.out.println(output);
+        }
+    }
+}
+//Accepted and pushed
