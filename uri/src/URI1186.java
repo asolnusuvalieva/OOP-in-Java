48a1be99b023f0dbff76389c1c6f2f7cf8dfe43d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-19 23:33:25 +0600

    Do URI1186

---
+++ uri/src/URI1186.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class URI1186 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        double[][] table = new double[12][12];
+        double sum = 0;
+        double count = 0;
+        for(int i = 0; i < table.length; i++) {
+            int diagonalIndex = table[i].length - 1 - i;
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = scanner.nextDouble();
+
+                if(j > diagonalIndex){
+                    sum += table[i][j];
+                    count++;
+                }
+            }
+        }
+
+        if(s.equals("S")){
+            System.out.printf("%.1f%n", sum);
+        }else{
+            double average = sum/count;
+            System.out.printf("%.1f%n", average);
+        }
+    }
+}
+//Accepted and pushed
