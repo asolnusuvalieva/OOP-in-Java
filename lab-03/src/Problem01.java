ce13f32c384666012fb44b8805cccfddd67bb27f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-25 22:53:35 +0600

    Do Problem01 in lab-03

---
+++ lab-03/src/Problem01.java
@@ -0,0 +1,10 @@
+public class Problem01 {
+    public static void main(String[] args) {
+        //TODO: do input processing
+        Rational num1 = new Rational(1, 2);
+        Rational num2 = new Rational(2, 4);
+        Rational result = num1.add(num2);
+
+        System.out.println(result);
+    }
+}
