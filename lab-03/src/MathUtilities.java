8b772c8f45e8c089a42b3582b94cc5ab4286cb00
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-25 22:54:13 +0600

    Include MathUtilities.java file in lab-03

---
+++ lab-03/src/MathUtilities.java
@@ -0,0 +1,17 @@
+public class MathUtilities {
+    private MathUtilities(){} //for prohibiting constructing objects of this type
+
+    static int gcd(int a, int b){
+        if(a == 0 && b == 0){
+            throw new IllegalArgumentException("GCD is not defined for 0 and 0");
+        }
+        int temp = a % b;
+        while (temp != 0){
+            a = b;
+            b = temp;
+            temp = a % b;
+        }
+
+        return b;
+    };
+}
