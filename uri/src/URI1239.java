d6cd6eccae6811bbb8f86c3eaf278be0b2a99b12
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-27 21:11:12 +0600

    Do URI1239

---
+++ uri/src/URI1239.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/*
+Translating shortcuts into HTML tags!
+There are italic or bold shortcuts;
+The number of underscores is even,
+The number of asterisks is even.
+
+Any substring of the text that are enclosed by two underscores or asterisks CANNOT contain more further.
+ */
+public class URI1239 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextLine()){
+            String text = scanner.nextLine();
+            char[] chars = text.toCharArray();
+            int numberOfUnderscores = 0;
+            int numberOfAsterisks = 0;
+
+            for(char currentChar : chars){
+                if(currentChar == '_'){
+                    numberOfUnderscores++;
+                    System.out.print(numberOfUnderscores % 2 == 1 ? "<i>" : "</i>");
+                }else if (currentChar == '*'){
+                    numberOfAsterisks++;
+                    System.out.print(numberOfAsterisks % 2 == 1 ? "<b>" : "</b>");
+                }else{
+                    System.out.print(currentChar);
+                }
+            }
+            System.out.println(); //Putting cursor to the next line
+        }
+    }
+}
+//Accepted and pushed
