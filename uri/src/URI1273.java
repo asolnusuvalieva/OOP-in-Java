a7705a06875aaa5a2678df5a0fab7d862ea1fcd8
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-01 00:31:29 +0600

    Do URI1273

--- uri/src/URI1273.java
+++ uri/src/URI1273.java
@@ -5,21 +5,26 @@ Goal is to right justify given words in the same order as they appear in the inp
 public class URI1273 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        boolean passed = false;
         int numberOfWords = scanner.nextInt();
+
         while (numberOfWords != 0){
             String[] words = new String[numberOfWords];
 
             //Reading words and writing into array
             for(int i = 0; i < words.length; i++){
-                words[i] = scanner.next();
+                words[i] = scanner.next().trim();
             }
 
             int maxLength = findMaxLength(words);
             for(int i = 0; i < words.length; i++){
+                if (i == 0 && passed){
+                    System.out.println();
+                }
                 System.out.println(makeOutput(maxLength, words[i]));
             }
 
+            passed = true;
             //Updating numberOfWords
             numberOfWords = scanner.nextInt();
         }
@@ -37,15 +42,15 @@ public class URI1273 {
     }
     public static int findMaxLength(String[] words){
         int maxLength = words[0].length();
-        int indexOfMax = 0;
 
         for(int i = 1; i < words.length; i++){
             if(words[i].length() > maxLength){
                 maxLength = words[i].length();
-                indexOfMax = i;
             }
         }
 
         return maxLength;
     }
 }
+
+//Accepted and pushed
