f9a5a386e83ca9e56ae2ab027c806bae2dfe1773
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-05 23:54:28 +0600

    Do URI1050

---
+++ uri/src/URI1050Fall.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class URI1050Fall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int codeNum = scanner.nextInt();
+        String destination = "";
+
+        switch (codeNum){
+            case 61: destination = "Brasilia"; break;
+            case 71: destination = "Salvador"; break;
+            case 11: destination = "Sao Paulo"; break;
+            case 21: destination = "Rio de Janeiro"; break;
+            case 32: destination = "Juiz de Fora"; break;
+            case 19: destination = "Campinas"; break;
+            case 27: destination = "Vitoria"; break;
+            case 31: destination = "Belo Horizonte"; break;
+            default: destination = "DDD nao cadastrado"; break;
+        }
+
+        System.out.println(destination);
+    }
+}
+//accepted
