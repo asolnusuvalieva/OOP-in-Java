a7b77817953e917e9f2497bbd5fb503ca0d34acc
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-06 23:55:17 +0600

    Do URI2356

---
+++ uri/src/URI2356.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/*
+Question: If bacteria resistant or not?
+- DNA
+- Part of the genetic code
+ */
+public class URI2356 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()){
+            String DNA = scanner.nextLine();
+            String resistanceGeneticCode = scanner.nextLine();
+
+            if(DNA.contains(resistanceGeneticCode)){
+                System.out.println("Resistente");
+            }else{
+                System.out.println("Nao resistente");
+            }
+        }
+    }
+}
+//Accepted and pushed
