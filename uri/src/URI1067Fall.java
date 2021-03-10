68c3d376fc0f40db2a23f3cb60a2aa569846f72e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-10 23:13:19 +0600

    Do URI1067

---
+++ uri/src/URI1067Fall.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class URI1067Fall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        for(int i = 1; i <= x; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+//Accepted and pushed
