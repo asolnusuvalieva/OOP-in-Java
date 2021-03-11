86ac6315a95d8f0747790b384964e08ccc5c29b7
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-11 23:28:43 +0600

    Do URI1095

---
+++ uri/src/URI1095Fall.java
@@ -0,0 +1,14 @@
+public class URI1095Fall {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 60;
+
+        while (j >= 0){
+            System.out.print("I=" + i + " ");
+            System.out.println("J=" + j);
+            i += 3;
+            j -= 5;
+        }
+    }
+}
+//accepted
