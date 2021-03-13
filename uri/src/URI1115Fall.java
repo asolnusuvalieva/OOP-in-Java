cb707496126b0b20a50410169c1d58231b217839
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-13 22:45:08 +0600

    Do URI1115

---
+++ uri/src/URI1115Fall.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/*
+primeiro - I
+segundo - II
+terceiro - III
+quarto - IV
+ */
+public class URI1115Fall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        while (x != 0 || y != 0){
+            if(x > 0 && y > 0){ //I
+                System.out.println("primeiro");
+            }else if(x < 0 && y > 0){
+                System.out.println("segundo");
+            }else if(x < 0 && y < 0){
+                System.out.println("terceiro");
+            }else{
+                System.out.println("quarto");
+            }
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+        }
+    }
+}
+
+//accepted
