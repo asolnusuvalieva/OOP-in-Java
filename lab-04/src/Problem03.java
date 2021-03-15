a33fd4ba79a350bd1413e0db9e3f0e58a65b362c
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-15 20:04:48 +0600

    Do Problem03 in lab-04

---
+++ lab-04/src/Problem03.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayListIntN numbers = new ArrayListIntN();
+        while (scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            numbers.add(number); //after each iteration, the array will grow
+        }
+
+        System.out.println("Before reversing:");
+        System.out.println(numbers); //toString()
+
+        System.out.println("After reversing:");
+        reverse(numbers);
+        System.out.println(numbers); //toString()
+
+        addZerosBeforeEvenValues(numbers);
+        System.out.println("After insertion:");
+        System.out.println(numbers); //toString()
+
+        removeOddValues(numbers);
+        System.out.println("After deletions:");
+        System.out.println(numbers); //toString()
+    }
+
+    private static void reverse(ArrayListIntN numbers){
+        for(int i = 0, j = numbers.size() - 1; i < j; i++, j--){
+            int temp = numbers.get(i); //reading an information, automatic boxing
+            numbers.set(i, numbers.get(j)); //writing/replacing
+            numbers.set(j, temp);
+        }
+    }
+
+    /*Problem02*/
+
+    private static void addZerosBeforeEvenValues(ArrayListIntN numbers){
+        //inserts value 0 before each even number
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) % 2 == 0){
+                numbers.add(i, 0);//inserting a new value
+                i++; //to skip the element we just analyzed
+            }
+        }
+    }
+
+    private static void removeOddValues(ArrayListIntN numbers){
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) % 2 != 0){
+                numbers.remove(i);
+                i--; //not to skip the next value
+            }
+        }
+    }
+}
