b316e81d788387e5ca3ebdab39d429146fd35a75
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-15 20:04:17 +0600

    Implement ArrayListInt class

---
+++ lab-04/src/ArrayListIntN.java
@@ -0,0 +1,75 @@
+/*
+Naive Solution
+ */
+public class ArrayListIntN {
+    private int[] data;
+
+    ArrayListIntN(){
+        data = new int[0];
+    }
+
+    int size(){
+        return data.length;
+    }
+
+    int get(int index){
+        return data[index]; //Java itself will raise Errors in case of incorrect input
+    }
+    void set(int index, int value){
+        data[index] = value; //Java itself will raise Errors in case of incorrect input
+    }
+
+    void add(int value){ //Appending
+        int[] temp = new int[data.length + 1];
+
+        //Copying old values to the new array
+        for(int i = 0; i < data.length; i++){
+            temp[i] = data[i];
+        }
+        temp[temp.length - 1] = value;
+        data = temp;
+    }
+
+    void add(int index, int value){ //Inserting
+        if(index < 0 || index >= data.length){
+            throw new IndexOutOfBoundsException("Invalid index of insertion!");
+        }
+
+        int[] temp = new int[data.length + 1];
+
+        for(int d = 0, t = 0; d < data.length; d++, t++){
+            if(t == index){
+                t++;
+            }
+            temp[t] = data[d];
+        }
+        temp[index] = value;
+
+        data = temp;
+    }
+    void remove(int index){
+        if(index < 0 || index >= data.length){
+            throw new IndexOutOfBoundsException("Invalid index of removal!");
+        }
+
+        int[] temp = new int[data.length - 1];
+        for(int d = 0, t = 0; d < data.length; d++, t++){
+            if(d == index){
+                d++;
+            }
+            temp[t] = data[d];
+        }
+
+        data = temp;
+    }
+
+    public String toString(){
+        StringBuilder result = new StringBuilder("[");
+
+        for(int i = 0; i < data.length; i++){
+            result.append(data[i]).append((i != data.length - 1 ? ", " : "]"));
+        }
+
+        return result.toString();
+    }
+}
