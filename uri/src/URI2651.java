d981cf7be3adf93f5ab590c584ba050c9e35557d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-21 23:31:43 +0600

    Do URI2651

---
+++ uri/src/URI2651.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class URI2651 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String link = "zelda";
+
+        while (scanner.hasNext()){
+            String letter = scanner.next().toLowerCase();
+
+            if(letter.contains(link)){
+                System.out.println("Link Bolado");
+            }else{
+                System.out.println("Link Tranquilo");
+            }
+        }
+    }
+}
+//Accepted and pushed
