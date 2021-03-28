71d2dbdc15b2dfb5bd3096d9003ce641f3683243
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-03-28 23:39:19 +0600

    Implement isValid(int[][] solution) and isValid(int i, int j, int[][] solution)  methods in 'Sudoku' Ex

--- BookExercises/src/Sudoku.java
+++ BookExercises/src/Sudoku.java
@@ -15,4 +15,43 @@ public class Sudoku {
 
         return grid;
     }
+
+    //is Solution Valid?
+    public static boolean isValid(int[][] solution){
+        for(int i = 0; i < solution.length; i++){
+            for(int j = 0; j < solution[i].length; j++){
+                if(solution[i][j] < 1 || solution[i][j] > 9 || !isValid(i, j, solution)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //is Cell unique?
+    public static boolean isValid(int i, int j, int[][] solution){
+        //Unique in row "i"?
+        for(int column = 0; column < solution[i].length; column++){
+            if(solution[i][column] == solution[i][j] && column != j){
+                return false;
+            }
+        }
+
+        //Unique in column "j"
+        for(int row = 0; row < solution.length; row++){
+            if(row != i && solution[i][j] == solution[row][j]){
+                return false;
+            }
+        }
+
+        //Unique in 3 X 3 block
+        for(int row = i/3 * 3; row < i/3 * 3 + 3; row++){
+            for(int col = j/3 * 3; col < j/3 * 3 + 3; col++){
+                if(row != i && col != j && solution[row][col] == solution[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
