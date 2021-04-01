ee89059acbca539e0eff073468876bbd4b375b73
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-01 23:57:41 +0600

    Implement the logic in 'TheSmallestFactor' ex

---
+++ BookExercises/src/TheSmallestFactor.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TheSmallestFactor {
+    public static void main(String[] args) {
+        int factor = 2;
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+/*
+        while (factor <= number){
+            if(number % factor == 0){
+                break;
+            }
+            factor++;
+        }
+
+ */
+        boolean found = false;
+        while (factor <= number && !found){
+            if(number % factor == 0){
+                found = true;
+            }else{
+                factor++;
+            }
+            /*
+            Если убрать else, то логика условия будет правильной, но зато к factor добавится ненужный 1.
+             */
+        }
+    }
+}
