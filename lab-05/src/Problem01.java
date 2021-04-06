6fff4c934ec83274dbefc3c61f948734fbfb74c0
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-06 23:19:17 +0600

    Add the buttons to the toolbarPanel

--- lab-05/src/Problem01.java
+++ lab-05/src/Problem01.java
@@ -18,6 +18,10 @@ public class Problem01 extends JFrame {
         JButton redButton = new JButton("RED"); //a text on a button
         JButton greenButton = new JButton("GREEN"); //a text on a button
         JButton blueButton = new JButton("BLUE"); //a text on a button
+
+        toolbarPanel.add(redButton);
+        toolbarPanel.add(greenButton);
+        toolbarPanel.add(blueButton);
     }
 
 
