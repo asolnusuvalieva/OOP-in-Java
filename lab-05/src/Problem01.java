3d7a46f389b5325aaa9cbcd1699fc133afbbcfe7
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-06 23:17:20 +0600

    Configure the window

--- lab-05/src/Problem01.java
+++ lab-05/src/Problem01.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Problem01 {
+public class Problem01 extends JFrame {
     Problem01(){
-
+        setSize(400, 400);
+        setLocationRelativeTo(null); //will center the window
+        setTitle("Sol: First GUI App");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //request to close the window on tapping [x]
+        setLayout(new BorderLayout());
     }
 
 
