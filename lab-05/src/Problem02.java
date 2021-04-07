47e470ffcf0c29f6adf48a54a9ebefa2df4641d8
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-07 23:29:50 +0600

    Create a window

--- lab-05/src/Problem02.java
+++ lab-05/src/Problem02.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Problem02 extends JFrame {
     Problem02(){
-
+        setSize(400, 400);
+        setLocationRelativeTo(null); //will center the window
+        setTitle("Sol: First GUI App");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //request to close the window on tapping [x]
+        setLayout(new BorderLayout());
     }
 }
