970551a9f2e247bc1e64683232ce7919b6584462
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-07 23:36:08 +0600

    Add mouseListeners to each button

--- lab-05/src/Problem02.java
+++ lab-05/src/Problem02.java
@@ -31,8 +31,14 @@ public class Problem02 extends JFrame {
         toolbarPanel.setBackground(Color.DARK_GRAY);
 
         JButton redButton = new JButton("RED"); //a text on a button
+        redButton.addMouseListener(new ColorChanger(mainPanel, Color.RED));
+
         JButton greenButton = new JButton("GREEN"); //a text on a button
+        greenButton.addMouseListener(new ColorChanger(mainPanel, Color.GREEN));
+
         JButton blueButton = new JButton("BLUE"); //a text on a button
+        blueButton.addMouseListener(new ColorChanger(mainPanel, Color.BLUE));
+
 
         toolbarPanel.add(redButton);
         toolbarPanel.add(greenButton);
