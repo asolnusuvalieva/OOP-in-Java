1efe9a32ef726378ed562139ad67198d1d2f3a8f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-15 21:29:36 +0600

    Start URI1087

---
+++ uri/src/batch6/URI1087.java
@@ -0,0 +1,9 @@
+package batch6;
+/*
+A chess problem:
+given the position of a queen in an empty standard chess board (that is, an 8 x 8 board)
+how many moves are needed so that she reaches another given square in the board?
+ */
+public class URI1087 {
+
+}
