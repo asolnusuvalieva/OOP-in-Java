96b1fd80a1f86af60ca3e9e2f6b6b6155aa7ee2d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-16 22:15:47 +0600

    Finish URI1087

--- uri/src/batch6/URI1087.java
+++ uri/src/batch6/URI1087.java
@@ -14,52 +14,17 @@ public class URI1087{
             int y1 = Integer.parseInt(parts[1]);
             int x2 = Integer.parseInt(parts[2]);
             int y2 = Integer.parseInt(parts[3]);
-        }
-    }
-    private static boolean isSameDiagonal(int x1, int y1, int x2, int y2){
-        boolean isSameDiagonal = false;
-        boolean firstQ = x2 > x1 && y2 < y1; // [x1 + 1, 8], [1, y1 - 1]
-        boolean secondQ = x2 < x1 && y2 < y1; // [1, x1 - 1], [1, y1 - 1]
-        boolean thirdQ = x2 < x1 && y2 > y1; // [1, x1 -1], [y1 + 1, 8]
-        boolean forthQ = x2 > x1 && y2 > y1; // [x1 + 1, 8], [y1 + 1, 8]
 
-        int startBoardY = -1;
-        int endBoardY = -1;
-        int startBoardX = -1;
-        int endBoardX = -1;
-
-        if(firstQ){
-            startBoardY = 1;
-            endBoardY = y1 - 1;
-            startBoardX = x1 + 1;
-            endBoardX = 8;
-        }else if(secondQ){
-            startBoardY = 1;
-            endBoardY = y1 - 1;
-            startBoardX = 1;
-            endBoardX = x1 - 1;
-        }else if(thirdQ){// [1, x1 -1], [y1 + 1, 8]
-            startBoardY = y1 + 1;
-            endBoardY = 8;
-            startBoardX = 1;
-            endBoardX = x1 - 1;
-        }else if(forthQ){// [x1 + 1, 8], [y1 + 1, 8]
-            startBoardY = y1 + 1;
-            endBoardY = 8;
-            startBoardX = x1 + 1;
-            endBoardX = 8;
-        }
-
-        //Check individual points on the diagonal of that quarter
-        for(int boardY = startBoardY; boardY <= endBoardY; boardY++){
-            for(int boardX = startBoardX; boardX <= endBoardX; boardX++){
-                if(boardX == x2 && boardY == y2){
-                    isSameDiagonal = true;
-                    break;
-                }
+            if(x1 == x2 && y1 == y2){
+                System.out.println(0);
+            }else if(x1 == x2 || y1 == y2 || (Math.abs(x1 - x2) == Math.abs(y1 - y2))){
+                System.out.println(1);
+            }else{
+                System.out.println(2);
             }
-        }
 
-        return isSameDiagonal;
+            input = scanner.nextLine(); //Accepting new input
+        }
     }
 }
+//Accepted
