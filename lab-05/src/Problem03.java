baa0e2043ceda2e75fa41ddbe4685a5f580119d4
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-19 23:57:55 +0600

    Create a class CoordinateReporter in Problem03?

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -1,10 +1,34 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
 class Reporter{
     private final JFrame frame;
 
     public Reporter(JFrame frame){
         this.frame = frame;
     }
+}
+
+class CoordinateReporter extends Reporter implements MouseMotionListener {
+    public CoordinateReporter(JFrame frame){
+        super(frame);
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+     /*
+    Options for mouseMoved()
+    - MouseAdapter (abstract class which implements MouseMotionListener, ...)
+    - MouseMotionListener (interface)
+     */
 }
 
 public class Problem03 {
