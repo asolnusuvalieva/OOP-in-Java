69e4ed456fb036753e8dab515cf69a919ca8f326
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-20 23:31:18 +0600

    Add static final properties to the Class Canvas

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 
 class Canvas extends JPanel {
-
+    private static final int FIELD_WIDTH = 10;
+    private static final int FIELD_HEIGHT = 10;
+    private static final float CELL_GAP = 0.85f;
 }
 public class Problem04 {
 }
