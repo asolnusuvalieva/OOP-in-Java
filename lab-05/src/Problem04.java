b1c7256ffa6d50c9abd76dca057c8ad09711ddcc
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-20 23:32:04 +0600

    Override the paintComponent() method

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 class Canvas extends JPanel {
     private static final int FIELD_WIDTH = 10;
     private static final int FIELD_HEIGHT = 10;
     private static final float CELL_GAP = 0.85f;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+    }
+
+
+
+
 }
 public class Problem04 {
 }
