98496d2b05e84c98da5fc4f0f89e2c6695eca439
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-20 23:32:32 +0600

    Add local variables

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -8,7 +8,13 @@ class Canvas extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
+        super.paintComponent(g); //let the parent fill the background with some color
 
+        int cellSize = (int)(Math.min(getWidth()/FIELD_WIDTH, getHeight()/FIELD_HEIGHT) * CELL_GAP); //80% is just to have gaps around the board
+        int screenFieldWidth = FIELD_WIDTH * cellSize;
+        int screenFieldHeight = FIELD_HEIGHT * cellSize;
+        int centeringShiftX = (int)((getWidth() - screenFieldWidth)/2f);
+        int centeringShiftY = (int)((getHeight() - screenFieldHeight)/2f);
     }
 
 
