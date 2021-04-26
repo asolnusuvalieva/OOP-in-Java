aa9908ec40e118aac731b870b843b3a83b11189f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-26 23:45:49 +0600

    Add two constructors

--- lab-05/src/Problem05.java
+++ lab-05/src/Problem05.java
@@ -3,6 +3,15 @@ class Field{
     private static final int DEFAULT_FIELD_HEIGHT = 10;
 
     private int width, height;
+
+    Field(){
+        this(DEFAULT_FIELD_WIDTH, DEFAULT_FIELD_HEIGHT);
+    }
+
+    Field(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
 }
 public class Problem05 {
 }
