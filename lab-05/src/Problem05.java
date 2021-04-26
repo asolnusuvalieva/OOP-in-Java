7cb0cee9c2fd0b0a08339ac9f02bc62866725645
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-26 23:44:56 +0600

    Add data fields

---
+++ lab-05/src/Problem05.java
@@ -0,0 +1,8 @@
+class Field{
+    private static final int DEFAULT_FIELD_WIDTH = 10;
+    private static final int DEFAULT_FIELD_HEIGHT = 10;
+
+    private int width, height;
+}
+public class Problem05 {
+}
