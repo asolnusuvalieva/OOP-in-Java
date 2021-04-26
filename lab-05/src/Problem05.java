b7aa64406ef8b72a262fd685e770c0bc336a1703
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-26 23:47:36 +0600

    Add areCoordsInside() method

--- lab-05/src/Problem05.java
+++ lab-05/src/Problem05.java
@@ -19,6 +19,11 @@ class Field{
     int getHeight(){
         return height;
     }
+
+    boolean areCoordsInside(int x, int y){
+        return x >= 0 && x < width &&
+                y >= 0 && y < height;
+    }
 }
 public class Problem05 {
 }
