28cf8457abc268870d87b8d325276a2a0821e03f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-15 03:55:36 +0600

    Implement Player class

---
+++ project-01-oop-console/src/Player.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Player {
+    public boolean lost = false;
+    public boolean won;
+    public int totalCellsToUncover;
+
+
+    public Player(GameLevel gameLevel){
+        totalCellsToUncover = gameLevel.getFIELD_HEIGHT() * gameLevel.getFIELD_WIDTH() - gameLevel.getMINES();
+        won = totalCellsToUncover == 0;
+    }
+
+    public static int chooseDifficulty(Scanner scanner) throws EOFException{
+        do{
+            System.out.println(
+                    "Select difficulty:\n" +
+                            "\t1. Beginner\n" +
+                            "\t2. Intermediate\n" +
+                            "\t3. Expert"
+            );
+
+            if(!scanner.hasNextLine()){
+                throw new EOFException("End Of Input");
+            }
+
+            try{
+                String rawInput = scanner.nextLine();
+                int input = Integer.parseInt(rawInput);
+                if(input < 1 || input > 3){
+                    throw new NumberFormatException();
+                }
+                return input;
+            }catch (NumberFormatException e){
+                System.err.print("Invalid difficulty level. Try again (or Command+D to exit).");
+            }
+        }while (true);
+    }
+
+    public static int[] enterCoordinates(Scanner scanner, int minX, int maxX, int minY, int maxY) throws EOFException{
+        do{
+            System.out.println("Select a cell (x, y): ");
+
+            if(!scanner.hasNextLine()){
+                throw new EOFException("End Of Input");
+            }
+
+            try{
+                String[] rawInput = scanner.nextLine().trim().split("\\s+");;
+                if(rawInput.length != 2){
+                    throw new NumberFormatException();
+                }
+                int x = Integer.parseInt(rawInput[0]);
+                x--;
+                if(x < minX || x >= maxX){
+                    throw new NumberFormatException();
+                }
+
+                int y = Integer.parseInt(rawInput[1]);
+                y--;
+                if(y < minY || y >= maxY){
+                    throw new NumberFormatException();
+                }
+
+                return new int[] {x, y};
+            }catch (NumberFormatException e){
+                System.err.print("Invalid coordinates. Try again (or Command+D to exit).");
+            }
+        }while (true);
+    }
+}
