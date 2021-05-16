0e73ec6bd78bfd432472a015e34423f96531c1c1
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-16 22:23:45 +0600

    Add Label class

---
+++ project-01-oop-gui/src/PButton/Label.java
@@ -0,0 +1,34 @@
+package PButton;
+public class Label {
+    private final String text; //text on the top of the button
+    private final int fontSize = 16;
+
+    private final int color = 0xffe0e0e0;
+    private final int colorHover = 0xffe0e0e0;
+    private final int colorActive = 0xff969696;
+
+    public Label(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public int getColorHover() {
+        return colorHover;
+    }
+
+    public int getColorActive() {
+        return colorActive;
+    }
+}
+
