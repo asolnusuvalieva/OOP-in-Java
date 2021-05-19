2b8dedc473d8c039445be15574050dca316e7656
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-20 02:31:12 +0600

    Finish Problem02 in lab-05

--- lab-05/src/Problem02.java
+++ lab-05/src/Problem02.java
@@ -22,7 +22,7 @@ public class Problem02 extends JFrame {
         /* WINDOW */
         setSize(400, 400);
         setLocationRelativeTo(null); //set the window to the center of the screen
-        setTitle("Lab-05: Problem01");
+        setTitle("Lab-05: Problem02");
         setDefaultCloseOperation(EXIT_ON_CLOSE); //What to do by default when we close the window? //it'll stop the program
         setLayout(new BorderLayout()); //Choose a layout engine
 
