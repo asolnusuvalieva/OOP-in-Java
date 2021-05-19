43156488bcb0dcae7f43a96eacb981a055a6e893
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-19 21:45:37 +0600

    Finish Problem02 in lab-05

--- lab-05/src/Problem02.java
+++ lab-05/src/Problem02.java
@@ -6,49 +6,61 @@ import java.awt.event.MouseEvent;
 class ColorChanger extends MouseAdapter {
     private final JPanel panel;
     private final Color color;
-
-    ColorChanger(JPanel panel, Color color){
+    public ColorChanger(JPanel panel, Color color){
         this.panel = panel;
         this.color = color;
     }
-
+    @Override
     public void mouseClicked(MouseEvent e) {
         panel.setBackground(color);
     }
 }
+
 public class Problem02 extends JFrame {
     Problem02(){
+        /* WINDOW */
         setSize(400, 400);
-        setLocationRelativeTo(null); //will center the window
-        setTitle("Sol: First GUI App");
-        setDefaultCloseOperation(EXIT_ON_CLOSE); //request to close the window on tapping [x]
-        setLayout(new BorderLayout());
+        setLocationRelativeTo(null); //set the window to the center of the screen
+        setTitle("Lab-05: Problem01");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //What to do by default when we close the window? //it'll stop the program
+        setLayout(new BorderLayout()); //Choose a layout engine
 
+        /* Red Panel */
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(Color.RED);
+        /* Adding the panel to the window */
+        add(mainPanel, "Center");
 
+        /* Creating Buttons */
+        JButton redButton = new JButton("RED"); //a button with a text/name
+        redButton.addMouseListener(new ColorChanger(mainPanel, Color.RED)); //inside, it will call mouseClicked() of ColorChanger cb it's just a MouseAdapter
+
+        JButton greenButton = new JButton("GREEN"); //a button with a text/name
+        greenButton.addMouseListener(new ColorChanger(mainPanel, Color.GREEN));
+
+        JButton blueButton = new JButton("BLUE"); //a button with a text/name
+        blueButton.addMouseListener(new ColorChanger(mainPanel, Color.BLUE));
+
+        /* Toolbar Panel */
         JPanel toolbarPanel = new JPanel();
         toolbarPanel.setBackground(Color.DARK_GRAY);
 
-        JButton redButton = new JButton("RED"); //a text on a button
-        redButton.addMouseListener(new ColorChanger(mainPanel, Color.RED));
-
-        JButton greenButton = new JButton("GREEN"); //a text on a button
-        greenButton.addMouseListener(new ColorChanger(mainPanel, Color.GREEN));
-
-        JButton blueButton = new JButton("BLUE"); //a text on a button
-        blueButton.addMouseListener(new ColorChanger(mainPanel, Color.BLUE));
-
-
+        /* Adding buttons to the panel */
         toolbarPanel.add(redButton);
         toolbarPanel.add(greenButton);
         toolbarPanel.add(blueButton);
 
-        add(mainPanel, "Center");
+        /* Adding the panel to the window */
         add(toolbarPanel, "South");
     }
 
     public static void main(String[] args) {
-        new Problem02().setVisible(true); //set window visible and start an infinite loop
+        new Problem02().setVisible(true);
+        /*
+        So Problem01 is basically a JFrame (a window) ;)
+        It sets the window visible and start an event loop that doesn't end when reaching the {}
+         */
     }
 }
+
+//Final Version
