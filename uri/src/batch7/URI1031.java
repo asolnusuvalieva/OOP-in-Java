aabdca2b27ca240b10eaefdd71848753efe9346d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-18 23:53:48 +0600

    Finish URI1031

---
+++ uri/src/batch7/URI1031.java
@@ -0,0 +1,19 @@
+package batch7;
+
+import java.util.Scanner;
+/*
+Country is divided into N regions (Auckland is #1, Wellington is #13).
+m is a random number. => It's all about the order of regions.
+
+Logic:
+Auckland is first
+Every m'th region after that
+
+Task is to come up with m so that 13 is the last region selected.
+ */
+public class URI1031 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int regions = scanner.nextInt();
+    }
+}
